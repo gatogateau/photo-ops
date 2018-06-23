8c5fa7818f5d2d22ab3358ea8e3b69e04d64c4e9
Jerson Hatch <vote4chubs@hotmail.com>
2018-06-23 10:35:47 -0600

    apis

---
+++ controllers/imageController.js
@@ -0,0 +1,7 @@
+// image controller for API calls to the cloudinary
+
+// createPic - post to cloudinary with userid or username
+
+// checkImage GET image taken by assassin and profile pic to make comparison
+
+// verifyImage - GET send target the assassin picture for verification
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,17 @@ module.exports = {
             .sort(name)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
-    }
+    },
+
+    // createUser  create new user - post
+
+    // findUser get user by username - GET
+
+    // updateUser if user dies, or eliminates target - if >10 kills - level up
+
+    // getUser gets user profile for profile page.  show stats and games involved with
+
+    
 
 
 };

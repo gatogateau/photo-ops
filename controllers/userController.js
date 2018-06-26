a3f68f5d25bf7f23eeca1dd54e616812ad44ab36
Jerson Hatch <vote4chubs@hotmail.com>
2018-06-26 13:44:12 -0600

    updates on controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,15 +10,32 @@ module.exports = {
             .catch(err => res.status(422).json(err));
     },
 
+       // find each current game by username  get
+       findgame: async function (req, res) {
+        
+        // req.query = username
+        // const user=username
+        console.log(req)
+        const user = await db.User.find(req.param, (err, user) => {
+            if (err) return err;
+            return user;
+          });
+          res.send(user.activeGames);
+        }
+    },
+
+
+
     // createUser  create new user - post
 
     // findUser get user by username - GET
+    // would I use this 
 
     // updateUser if user dies, or eliminates target - if >10 kills - level up
 
     // getUser gets user profile for profile page.  show stats and games involved with
 
-    
+
 
 
 };
